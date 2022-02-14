a78c878aa96786d3be25676e97c376a3869109ab
dannurahmat <rahmat.s.dannu@gmail.com>
2022-02-14 14:13:20 +0700

    - create factory - create seeder - create sample request

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomNumber(5),
+            'quantity' => $this->faker->randomNumber(3)
+        ];
+    }
+}
