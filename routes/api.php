95eefea60be6273624adc1f8597373d8049ed0bc
dannurahmat <rahmat.s.dannu@gmail.com>
2022-02-16 10:47:44 +0700

    add sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +18,29 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/auth', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('products', ProductController::class);
 
-Route::resource('products', ProductController::class);
+    Route::get('/logout', function(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response('Successfully logout');
+    });
+});
